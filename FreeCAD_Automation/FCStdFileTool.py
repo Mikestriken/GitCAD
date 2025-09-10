3500865f8d22de136d69b34a9c51a2fdf1a1ae99
Mikestriken <michael.john.marais379@gmail.com>
2025-09-10 14:21:26 -0400

    Added CLI flag to python script

--- FreeCAD_Automation/FCStdFileTool.py
+++ FreeCAD_Automation/FCStdFileTool.py
@@ -16,19 +16,31 @@ DEFAULT_CONFIG = {
     COMPRESS_NON_HUMAN: True
 }
 
-EXPORT_CMD:str = '--export'
-IMPORT_CMD:str = '--import'
+EXPORT_FLAG:str = '--export'
+IMPORT_FLAG:str = '--import'
+CLI_FLAG:str = '--CLI' # Ignores config file, just directly interface with the `from freecad import project_utility as PU` API
         
 def main():
+    # Setup CLI args
     parser:argparse.ArgumentParser = argparse.ArgumentParser(description="FreeCAD .FCStd file manager")
-    parser.add_argument(EXPORT_CMD, dest='export_flag', nargs=2, metavar=('INPUT_FILE', 'OUTPUT_DIR'), help='export files from .FCStd archive')
-    parser.add_argument(IMPORT_CMD, dest='import_flag', nargs=2, metavar=('INPUT_DIR', 'OUTPUT_FILE'), help='Create .FCStd archive from directory')
+    parser.add_argument(EXPORT_FLAG, dest='export_flag', nargs=2, metavar=('INPUT_FILE', 'OUTPUT_DIR'), help='export files from .FCStd archive')
+    parser.add_argument(IMPORT_FLAG, dest='import_flag', nargs=2, metavar=('INPUT_DIR', 'OUTPUT_FILE'), help='Create .FCStd archive from directory')
+    parser.add_argument(CLI_FLAG, dest="cli_flag", action='store_true', help='Use CLI mode, ignore configurations, user just interfaces with project_utility API')
 
     args = parser.parse_args()
     
-    with open(CONFIG_PATH, 'r') as f:
-        config = json.load(f)
+    # Store booleans used globally in easier to read bool variables
+    CLI_MODE:bool = args.cli_flag
+    INCLUDE_THUMBNAIL:bool = config.get(INCLUDE_THUMBNAILS, DEFAULT_CONFIG[INCLUDE_THUMBNAILS])
 
+    # Load config files
+    config = {}
+    
+    if not CLI_MODE:
+        with open(CONFIG_PATH, 'r') as f:
+            config = json.load(f)
+
+    # Main Logic
     if args.export_flag:
         input_file, output_dir = args.export_flag
         if not os.path.exists(output_dir):
@@ -36,7 +48,8 @@ def main():
 
         PU.extractDocument(input_file, output_dir)
 
-        if not config.get(INCLUDE_THUMBNAILS, DEFAULT_CONFIG[INCLUDE_THUMBNAILS]):
+        # Store thumbnail? logic
+        if not CLI_MODE and not INCLUDE_THUMBNAIL:
             thumbnails_dir = os.path.join(output_dir, 'thumbnails')
             if os.path.exists(thumbnails_dir):
                 shutil.rmtree(thumbnails_dir)
@@ -47,7 +60,8 @@ def main():
         input_dir, output_file = args.import_flag
         PU.createDocument(os.path.join(input_dir, 'Document.xml'), output_file)
 
-        if config.get(INCLUDE_THUMBNAILS, DEFAULT_CONFIG[INCLUDE_THUMBNAILS]):
+        # Store thumbnail? logic
+        if CLI_MODE or INCLUDE_THUMBNAIL:
             thumbnails_dir = os.path.join(input_dir, 'thumbnails')
 
             if os.path.exists(thumbnails_dir):
