a2218e4e675d0193568548360b9be58530beb146
Mikestriken <michael.john.marais379@gmail.com>
2025-09-11 12:11:44 -0400

    Fixed logic errors in refactoring added docstrings

--- FreeCAD_Automation/FCStdFileTool.py
+++ FreeCAD_Automation/FCStdFileTool.py
@@ -11,7 +11,7 @@ EXPORT_FLAG:str = '--export'
 IMPORT_FLAG:str = '--import'
 CLI_FLAG:str = '--CLI' # Ignores config file, just directly interface with the `from freecad import project_utility as PU` API
 
-def load_config_file(config_path: str) -> dict:
+def load_config_file(config_path:str) -> dict:
     """
     Redefines config file keys for this script.
     This way if the keys for the config file changes, it will not be necessary to update the keys throughout this entire script.
@@ -48,7 +48,17 @@ def load_config_file(config_path: str) -> dict:
         }
     }
         
-def construct_output_dir(FCStd_file_path: str, config: dict) -> str:
+def get_FCStd_dir_path(FCStd_file_path:str, config:dict) -> str:
+    """
+    Gets path to uncompressed FCStd file directory according to set configurations.
+
+    Args:
+        FCStd_file_path (str): Path to .FCStd file.
+        config (dict): Configurations dictionary.
+
+    Returns:
+        str: Path to uncompressed FCStd file directory.
+    """
     # Load relevant configs
     suffix:str = config['uncompressed_directory_structure']['uncompressed_directory_suffix']
     prefix:str = config['uncompressed_directory_structure']['uncompressed_directory_prefix']
@@ -60,11 +70,21 @@ def construct_output_dir(FCStd_file_path: str, config: dict) -> str:
     FCStd_file_name:str = os.path.splitext(os.path.basename(FCStd_file_path))[0] # remove .FCStd extension
     FCStd_constructed_dir_name:str = f"{prefix}{FCStd_file_name}{suffix}"
     
-    if USE_SUBDIR: return os.path.join(subdir_name, FCStd_constructed_dir_name)
+    if USE_SUBDIR: return os.path.join(FCStd_file_path, subdir_name, FCStd_constructed_dir_name)
     
-    else: return FCStd_constructed_dir_name
+    else: return os.path.join(FCStd_file_path, FCStd_constructed_dir_name)
 
-def construct_output_file(FCStd_dir_path: str, FCStd_file_path: str, config: dict) -> str:
+def get_FCStd_file_path(FCStd_dir_path:str, config:dict) -> str:
+    """
+    Gets path to .FCStd file according to set configurations.
+
+    Args:
+        FCStd_dir_path (str): Path to uncompressed FCStd file directory.
+        config (dict): Configurations dictionary.
+
+    Returns:
+        str: Path to .FCStd file.
+    """
     # Load relevant configs
     suffix:str = config['uncompressed_directory_structure']['uncompressed_directory_suffix']
     prefix:str = config['uncompressed_directory_structure']['uncompressed_directory_prefix']
@@ -73,14 +93,14 @@ def construct_output_file(FCStd_dir_path: str, FCStd_file_path: str, config: dic
     USE_SUBDIR:bool = config['uncompressed_directory_structure']['subdirectory']['put_uncompressed_directory_in_subdirectory']
     
     # Construct output path
-    FCStd_dir_name = os.path.basename(FCStd_dir_path).replace(suffix, '').replace(prefix, '')
+    FCStd_dir_name = os.path.basename(FCStd_dir_path).removesuffix(suffix).removeprefix(prefix)
     FCStd_constructed_file_name = f"{FCStd_dir_name}.FCStd"
     
-    if USE_SUBDIR: return os.path.join(subdir_name, FCStd_constructed_file_name, os.path.basename(FCStd_file_path))
+    if USE_SUBDIR: return os.path.join(FCStd_dir_path, "../..", FCStd_constructed_file_name)
     
-    else: return os.path.join(FCStd_constructed_file_name, os.path.basename(FCStd_file_path))
+    else: return os.path.join(FCStd_dir_path, "..", FCStd_constructed_file_name)
 
-def remove_export_thumbnail(FCStd_dir_path: str):
+def remove_export_thumbnail(FCStd_dir_path:str):
     """
     Remove thumbnail folder and contents from uncompressed FCStd file directory.
 
@@ -91,7 +111,7 @@ def remove_export_thumbnail(FCStd_dir_path: str):
     if os.path.exists(thumbnails_dir):
         shutil.rmtree(thumbnails_dir)
 
-def add_thumbnail_to_FCStd_file(FCStd_dir_path: str, FCStd_file_path: str):
+def add_thumbnail_to_FCStd_file(FCStd_dir_path:str, FCStd_file_path:str):
     """
     Add thumbnail to .FCStd file if uncompressed FCStd file directory has one.
 
@@ -104,6 +124,25 @@ def add_thumbnail_to_FCStd_file(FCStd_dir_path: str, FCStd_file_path: str):
         with zipfile.ZipFile(FCStd_file_path, 'a', zipfile.ZIP_DEFLATED) as zf:
             zf.write(thumbnail_path, 'thumbnails/Thumbnail.png')
 
+def bad_args(args:argparse.Namespace) -> bool:
+    """
+    Checks if CLI arguments are invalid. Returns True if invalid.
+
+    Args:
+        args (argparse.Namespace): CLI arguments to check.
+
+    Returns:
+        bool: True if invalid, else False.
+    """
+    
+    bad_arg_count:bool = True if args.export_flag and len(args.export_flag) > 2 or args.import_flag and len(args.import_flag) > 2 else False
+    
+    if bad_arg_count: return True
+    
+    missing_output_arg:bool = True if args.export_flag and len(args.export_flag) != 2 or args.import_flag and len(args.import_flag) != 2 else False
+    
+    if missing_output_arg and args.cli_flag: return True
+
 def main():
     # Setup CLI args
     parser:argparse.ArgumentParser = argparse.ArgumentParser(description="FreeCAD .FCStd file manager")
@@ -112,24 +151,29 @@ def main():
     parser.add_argument(CLI_FLAG, dest="cli_flag", action='store_true', help='Use CLI mode, ignore configurations, user just interfaces with project_utility API')
 
     args = parser.parse_args()
+    
+    if bad_args(args):
+        parser.print_help()
+        return
 
     # Load config file
+    config:dict
     if not args.cli_flag:
-        config: dict = load_config_file(CONFIG_PATH)
+        config:dict = load_config_file(CONFIG_PATH)
     else:
         config = {}
     
-    # Store booleans used globally in easier to read bool variables
-    INCLUDE_THUMBNAIL: bool = args.cli_flag or config.get('include_thumbnails', default=False)
+    
+    # I think that by default the thumbnail should be included if using the CLI.
+    INCLUDE_THUMBNAIL:bool = args.cli_flag or config.get('include_thumbnails', default=False)
     
     # Main Logic
     if args.export_flag:
         FCStd_file_path, FCStd_dir_path = args.export_flag
-        if not args.cli_flag:
-            FCStd_dir_path = construct_output_dir(FCStd_file_path, config)
         
-        if not os.path.exists(FCStd_dir_path):
-            os.makedirs(FCStd_dir_path)
+        if not args.cli_flag: FCStd_dir_path = get_FCStd_dir_path(FCStd_file_path, config)
+        
+        if not os.path.exists(FCStd_dir_path): os.makedirs(FCStd_dir_path)
 
         PU.extractDocument(FCStd_file_path, FCStd_dir_path)
 
@@ -141,7 +185,7 @@ def main():
     elif args.import_flag:
         FCStd_dir_path, FCStd_file_path = args.import_flag
         if not args.cli_flag:
-            FCStd_file_path = construct_output_file(FCStd_dir_path, FCStd_file_path, config)
+            FCStd_file_path = get_FCStd_file_path(FCStd_dir_path, config)
         
         os.makedirs(os.path.dirname(FCStd_file_path), exist_ok=True)
         
