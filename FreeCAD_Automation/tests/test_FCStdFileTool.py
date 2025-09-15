4070dac68558d3e5df0091b2615400072d144e38
Mikestriken <michael.john.marais379@gmail.com>
2025-09-14 20:53:43 -0400

    New import method

--- FreeCAD_Automation/tests/test_FCStdFileTool.py
+++ FreeCAD_Automation/tests/test_FCStdFileTool.py
@@ -1,13 +1,1 @@
-import sys
-import os
-
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../FreeCAD_Automation')))
-
-try:
-    # Module import that will fail. Left here for intellisense purposes
-    from ..FCStdFileTool import *
-    print_debug("Success")
-except ImportError as e:
-    # from FCStdFileTool import *
-    print(f"Error: {e}")
-    
+from ..FCStdFileTool import *
