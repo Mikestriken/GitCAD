cfb21da96cc3958df3c88579c9dcfe735ffe577a
Mikestriken <michael.john.marais379@gmail.com>
2025-09-19 12:37:30 -0400

    Initial repo tests commit

---
+++ FreeCAD_Automation/tests/run_repo_tests.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+# ==============================================================================================
+#                                  Verify and Retrieve Dependencies
+# ==============================================================================================
+# Check if inside a Git repository and ensure working dir is the root of the repo
+if ! git rev-parse --git-dir > /dev/null; then
+    echo "Error: Not inside a Git repository" >&2
+    exit 1
+fi
+
+GIT_ROOT=$(git rev-parse --show-toplevel)
+cd "$GIT_ROOT"
+
+# Import code used in this script
+FUNCTIONS_FILE="FreeCAD_Automation/functions.sh"
+source "$FUNCTIONS_FILE"
+
+CONFIG_FILE="FreeCAD_Automation/git-freecad-config.json"
+
+# Extract Python path
+PYTHON_PATH=$(get_freecad_python_path "$CONFIG_FILE") || exit 1
+
+# ==============================================================================================
+#                                          Get Binaries
+# ==============================================================================================
+git checkout test_binaries -- FreeCAD_Automation/tests/AssemblyExample.FCStd FreeCAD_Automation/tests/BIMExample.FCStd
+git add FreeCAD_Automation/tests/AssemblyExample.FCStd FreeCAD_Automation/tests/BIMExample.FCStd
+
+# ! git add executes the clean FCStd filter on the added .FCStd files.
+# ! Make sure to remove them after running the tests (don't commit them)
+
+# ==============================================================================================
+#                                           Run Tests
+# ==============================================================================================
