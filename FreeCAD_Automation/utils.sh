157545c735edf575f8b2ae74e2a1ed9e34327f05
Mikestriken <michael.john.marais379@gmail.com>
2025-09-23 14:43:56 -0400

    fixed realpath fail, added return success to get_FCStd_dir

--- FreeCAD_Automation/utils.sh
+++ FreeCAD_Automation/utils.sh
@@ -220,7 +220,9 @@ get_FCStd_dir() {
     }
 
     # Return the directory path (parent of lockfile)
-    realpath --canonicalize-missing --relative-to="$(git rev-parse --show-toplevel)" "$(dirname "$lockfile_path")"
+    realpath --canonicalize-missing --relative-to="$(git rev-parse --show-toplevel)" "$(dirname "$lockfile_path")" || return $FAIL
+
+    return $SUCCESS
 }
 
 # DESCRIPTION: Function to check if a directory has changes between two commits
