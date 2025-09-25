dd3072a68e38c8dfe1b4ba892c21e7a3f6f3e995
Mikestriken <michael.john.marais379@gmail.com>
2025-09-25 02:07:29 -0400

    uncommented valid lock debug prints

--- FreeCAD_Automation/utils.sh
+++ FreeCAD_Automation/utils.sh
@@ -153,14 +153,14 @@ FCStd_file_has_valid_lock() {
 
     # If locks not required, return valid
     if [ "$REQUIRE_LOCKS" == 0 ]; then
-        # echo "DEBUG: Locks not required, '$FCStd_file_path' lock is valid." >&2
+        echo "DEBUG: Locks not required, '$FCStd_file_path' lock is valid." >&2
         echo 1
         return $SUCCESS
     fi
 
     # File not tracked by git (new file), no lock needed (valid lock)
     if ! git ls-files --error-unmatch "$FCStd_file_path" > /dev/null 2>&1; then
-        # echo "DEBUG: New .FCStd file, '$FCStd_file_path' lock is valid." >&2
+        echo "DEBUG: New .FCStd file, '$FCStd_file_path' lock is valid." >&2
         echo 1
         return $SUCCESS
     fi
@@ -174,7 +174,7 @@ FCStd_file_has_valid_lock() {
 
     # Lockfile not tracked by git (new export), no lock needed (valid lock)
     if ! git ls-files --error-unmatch "$lockfile_path" > /dev/null 2>&1; then
-        # echo "DEBUG: New .FCStd file export, '$FCStd_file_path' lock is valid." >&2
+        echo "DEBUG: New .FCStd file export, '$FCStd_file_path' lock is valid." >&2
         echo 1
         return $SUCCESS
     fi
@@ -193,11 +193,11 @@ FCStd_file_has_valid_lock() {
     }
 
     if ! echo "$LOCK_INFO" | grep -q "$CURRENT_USER"; then
-        # echo "DEBUG: New .FCStd file export, '$FCStd_file_path' lock is INVALID." >&2
+        echo "DEBUG: New .FCStd file export, '$FCStd_file_path' lock is INVALID." >&2
         echo 0
         return $SUCCESS
     else
-        # echo "DEBUG: New .FCStd file export, '$FCStd_file_path' lock is valid." >&2
+        echo "DEBUG: New .FCStd file export, '$FCStd_file_path' lock is valid." >&2
         echo 1
         return $SUCCESS
     fi
