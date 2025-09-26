cb4d54e156f9f16d0f38d58a9369d201b2d527a3
Mikestriken <michael.john.marais379@gmail.com>
2025-09-25 20:29:45 -0400

    initial commit for FCStdStash.sh

---
+++ FreeCAD_Automation/FCStdStash.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# ==============================================================================================
+#                               Verify and Retrieve Dependencies
+# ==============================================================================================
+# Ensure working dir is the root of the repo
+# GIT_ROOT=$(git rev-parse --show-toplevel)
+# cd "$GIT_ROOT"
+
+# Import code used in this script
+FUNCTIONS_FILE="FreeCAD_Automation/utils.sh"
+source "$FUNCTIONS_FILE"
+
+if [ -z "$PYTHON_PATH" ] || [ -z "$REQUIRE_LOCKS" ]; then
+    echo "Config file missing or invalid; cannot proceed." >&2
+    exit $FAIL
+fi
+
+# ==============================================================================================
+#                                          Parse Args
+# ==============================================================================================
+
+# ==============================================================================================
+#                                   Execute Stash n' Import
+# ==============================================================================================
+
+exit $SUCCESS
