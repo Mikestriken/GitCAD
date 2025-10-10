274abbbfdcabb4f3ea138aec3d1d5a8f6759f790
Mikestriken <michael.john.marais379@gmail.com>
2025-10-10 14:44:08 -0400

    Added manual rebasing

--- FreeCAD_Automation/tests/run_repo_tests.sh
+++ FreeCAD_Automation/tests/run_repo_tests.sh
@@ -820,6 +820,7 @@ test_post_merge_hook() {
     assert_no_uncommitted_changes; echo
 
     echo "TEST: git pull --rebase origin active_test" >&2
+    confirm_user ">>>>>> Paused for manual user rebasing." "test_post_merge_hook" ""
     # For some reason linux likes to go into interactive rebase mode with no changes, requesting `git rebase --continue` command...
     if [[ "$OSTYPE" == "linux-gnu"* ]]; then
         echo "--- TEST: Rebasing and continuing for linux" >&2
