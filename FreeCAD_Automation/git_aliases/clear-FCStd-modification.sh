cdab1af31c843c5711f959c1efbf66e81fdd1198
Mikestriken <michael.john.marais379@gmail.com>
2025-12-11 13:33:57 -0500

    git fcmod should now work appropriately when user's pwd is in a subdir

--- FreeCAD_Automation/git_aliases/clear-FCStd-modification.sh
+++ FreeCAD_Automation/git_aliases/clear-FCStd-modification.sh
@@ -22,6 +22,47 @@ if [ -z "$PYTHON_PATH" ] || [ -z "$REQUIRE_LOCKS" ]; then
 fi
 
 # ==============================================================================================
+#                                          Parse Args
+# ==============================================================================================
+# CALLER_SUBDIR=${GIT_PREFIX}:
+    # If caller's pwd is $GIT_ROOT/subdir, $(GIT_PREFIX) = "subdir/"
+    # If caller's pwd is $GIT_ROOT, $(GIT_PREFIX) = ""
+CALLER_SUBDIR=$1
+shift
+
+# Parse remaining args: prepend CALLER_SUBDIR to paths (skip args containing '-')
+parsed_file_path_args=()
+while [ $# -gt 0 ]; do
+    # echo "DEBUG: parsing '$1'..." >&2
+    case $1 in
+        # Set boolean flag if arg is a valid flag
+        "-*")
+            echo "Error: '$1' flag is not recognized, skipping..." >&2
+            ;;
+        
+        # Assume arg is path. Fix path to be relative to root of the git repo instead of user's terminal pwd.
+        *)
+            if [ -n "$CALLER_SUBDIR" ]; then
+                case $1 in
+                    ".")
+                        # echo "DEBUG: '$1' -> '$CALLER_SUBDIR'" >&2
+                        parsed_file_path_args+=("$CALLER_SUBDIR")
+                        ;;
+                    *)
+                        # echo "DEBUG: prepend '$1'" >&2
+                        parsed_file_path_args+=("$CALLER_SUBDIR$1")
+                        ;;
+                esac
+            else
+                # echo "DEBUG: Don't prepend '$1'" >&2
+                parsed_file_path_args+=("$1")
+            fi
+            ;;
+    esac
+    shift
+done
+
+# ==============================================================================================
 #                                   Restore Staged FCStd files
 # ==============================================================================================
 # Get staged `.FCStd` files
@@ -36,4 +77,4 @@ fi
 # ==============================================================================================
 #                              Clear Modifications For Specified Files
 # ==============================================================================================
-RESET_MOD=$TRUE git add "$@"
+RESET_MOD=$TRUE git add "${parsed_file_path_args[@]}"
