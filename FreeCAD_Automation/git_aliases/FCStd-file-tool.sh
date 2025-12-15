68ada6d1dd1c89800c34317a39e358e808f7eb2b
Mikestriken <michael.john.marais379@gmail.com>
2025-12-15 13:45:07 -0500

    Reverted arg parsing of FCStd-file-tool.sh

--- FreeCAD_Automation/git_aliases/FCStd-file-tool.sh
+++ FreeCAD_Automation/git_aliases/FCStd-file-tool.sh
@@ -28,41 +28,29 @@ CALLER_SUBDIR=$1
 shift
 
 # Parse remaining args: prepend CALLER_SUBDIR to paths (skip args containing '-')
-parsed_file_path_args=()
-while [ $# -gt 0 ]; do
-    # echo "DEBUG: parsing '$1'..." >&2
-    case $1 in
-        # Set boolean flag if arg is a valid flag
-        -*)
-            echo "Error: '$1' flag is not recognized, skipping..." >&2
-            ;;
-        
-        # Assume arg is path. Fix path to be relative to root of the git repo instead of user's terminal pwd.
-        *)
-            if [ -n "$CALLER_SUBDIR" ]; then
-                case $1 in
-                    ".")
-                        # echo "DEBUG: '$1' -> '$CALLER_SUBDIR'" >&2
-                        parsed_file_path_args+=("$CALLER_SUBDIR")
-                        ;;
-                    *)
-                        # echo "DEBUG: prepend '$1'" >&2
-                        parsed_file_path_args+=("${CALLER_SUBDIR}${1}")
-                        ;;
-                esac
-            else
-                # echo "DEBUG: Don't prepend '$1'" >&2
-                parsed_file_path_args+=("$1")
-            fi
-            ;;
-    esac
-    shift
-done
+parsed_args=()
+if [ "$CALLER_SUBDIR" != "" ]; then
+    for arg in "$@"; do
+        case $arg in
+            -*)
+                parsed_args+=("$arg")
+                ;;
+            ".")
+                parsed_args+=("$CALLER_SUBDIR")
+                ;;
+            *)
+                parsed_args+=("${CALLER_SUBDIR}${arg}")
+                ;;
+        esac
+    done
+else
+    parsed_args=("$@")
+fi
 
 # ==============================================================================================
 #                                    Call FCStdFileTool.py
 # ==============================================================================================
 # Test to see how the args will be passed
-"$PYTHON_EXEC" "$FCStdFileTool" "${parsed_file_path_args[@]}"
+"$PYTHON_EXEC" "$FCStdFileTool" "${parsed_args[@]}"
 
 exit $SUCCESS
